00806cc94703cfc336f70b299f0501a49a284686
biubiubu98 <469460383@qq.com>
2023-12-31 11:06:36 +0800

    Site updated: 2023-12-31 11:06:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/30/hello-world/"];function toRandomPost(){
+var posts=["2023/12/30/日常文章/hello-world/","2023/12/31/日常文章/C-中的内存管理：深入理解与应用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
