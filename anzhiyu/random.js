38887e0cbedb6f516be586f8005436433e633cdd
biubiubu98 <469460383@qq.com>
2024-01-02 20:17:32 +0800

    Site updated: 2024-01-02 20:17:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/31/日常文章/C-中的内存管理：深入理解与应用/"];function toRandomPost(){
+var posts=["2023/12/31/日常文章/2023.12/C++中的内存管理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
