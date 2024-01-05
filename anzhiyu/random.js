6d1755d06f7daa20cc2959555099c114a500233b
biubiubu98 <469460383@qq.com>
2024-01-05 21:19:50 +0800

    Site updated: 2024-01-05 21:19:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/31/日常文章/2023.12/C++中的内存管理/","2024/01/04/directory/C语言目录/"];function toRandomPost(){
+var posts=["2023/12/31/日常文章/2023.12/C++中的内存管理/","2024/01/04/directory/C语言目录/","2024/01/05/日常文章/2024.1/C语言中变量的声明和定义深度解析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
