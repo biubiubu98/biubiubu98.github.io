6f86eb4276dcf5615477953eaa8122166a8f6568
biubiubu98 <469460383@qq.com>
2024-01-07 14:36:17 +0800

    Site updated: 2024-01-07 14:36:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/31/日常文章/2023.12/C++中的内存管理/","2024/01/04/directory/C语言目录/","2024/01/05/日常文章/2024.1/C语言中变量的声明和定义深度解析/","2024/01/07/C++项目/文章/1. C++控制台彩色时钟/","2024/01/04/directory/C++项目目录/"];function toRandomPost(){
+var posts=["2023/12/31/日常文章/2023.12/C++中的内存管理/","2024/01/04/directory/C语言目录/","2024/01/05/日常文章/2024.1/C语言中变量的声明和定义深度解析/","2024/01/04/directory/C++项目目录/","2024/01/07/Cpp项目/文章/1. C++控制台彩色时钟/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
